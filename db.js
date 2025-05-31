3e5efdffcbea17bee7180c77980f2e68573602f1
Jorge Altamirano <alojorge13@gmail.com>
2025-05-31 13:24:27 -0300

    agregando console.log a db.js

--- db.js
+++ db.js
@@ -17,7 +17,13 @@ export const pool = mysql.createPool({
   connectTimeout: 10000              // 👈 Aumenta timeout a 10s
 });
 
-
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
 const verifyconnection = async () => {
     let connection;
 

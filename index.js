7a2abeddbdef1e7f3f6c39daaa7e84b934d83c6c
Jorge Altamirano <alojorge13@gmail.com>
2025-05-31 13:46:17 -0300

    agregando PORT a mi index

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors({
   
 app.use('/api', router);
 
-const PORT = process.env.DB_PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Escuchando en el puerto: http://localhost:${PORT}`);
